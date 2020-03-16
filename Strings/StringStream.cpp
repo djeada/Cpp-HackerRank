7bfc192f496a28fe963c33fd33d6481924e5edb8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-03-16 15:54:13 +0100

    Strings

---
+++ Strings/StringStream.cpp
@@ -0,0 +1,30 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+     vector<int> integers;
+    int flag = 0;
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == ','){
+            integers.push_back(stoi(str.substr(flag,i)));
+            flag = i+1;
+        }
+    }
+    integers.push_back(stoi(str.substr(flag,str.size())));
+    return integers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
