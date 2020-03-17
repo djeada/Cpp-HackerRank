83e14f38bdce4f54befaa1462f94f3cc8d83d70e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-03-17 21:57:27 +0100

    STL

---
+++ STL/LowerBound.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> input_vector(int n){
+    vector<int> numbers;
+    int x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        numbers.push_back(x);
+    }
+    return numbers;
+}
+
+int main() {
+    int n, q, s;
+    cin >> n;
+    vector<int> numbers = input_vector(n);
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> s;
+        int index = (lower_bound(numbers.begin(), numbers.end(), s) - numbers.begin());
+        if (s == numbers[index]) {
+            cout << "Yes " << index + 1 << "\n";
+        } else {
+            cout << "No " << index + 1 << "\n";
+        }
+    }
+    return 0;
+}
---
+++ STL/VectorErase.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+vector<int> input_vector(int n){
+    vector<int> numbers;
+    int x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        numbers.push_back(x);
+    }
+    return numbers;
+}
+
+void print_vector(vector<int> arr){
+    for(int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    vector<int> numbers = input_vector(n);
+
+    int x;
+    cin >> x;
+    numbers.erase(numbers.begin()+x-1);
+
+    int a, b;
+    cin >> a >> b;
+    numbers.erase(numbers.begin()+a-1, numbers.begin()+b-1);
+
+    cout << numbers.size() << endl;
+    print_vector(numbers);
+  
+    return 0;
+}
